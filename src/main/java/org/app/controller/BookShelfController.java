f6fcaab3d3ce07596d806af0124d7daa55185604
Shpinkova_LA <liza.shpinkovaa@mail.ru>
2021-08-03 18:19:33 +0300

    added the ability to download different files

--- src/main/java/org/app/controller/BookShelfController.java
+++ src/main/java/org/app/controller/BookShelfController.java
@@ -4,19 +4,17 @@ import org.apache.log4j.Logger;
 import org.app.dto.Book;
 import org.app.exception.FilterOrRemoveByFieldException;
 import org.app.exception.UploadFileException;
+import org.app.repository.ProcessFileService;
 import org.app.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
@@ -27,10 +25,12 @@ public class BookShelfController {
 
     private final Logger logger = Logger.getLogger(BookShelfController.class);
     private final BookService bookService;
+    private final ProcessFileService fileService;
 
     @Autowired
-    public BookShelfController(BookService bookService) {
+    public BookShelfController(BookService bookService, ProcessFileService fileService) {
         this.bookService = bookService;
+        this.fileService = fileService;
     }
 
     @GetMapping("/shelf")
@@ -71,27 +71,19 @@ public class BookShelfController {
 
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        if (Objects.isNull(file) || file.isEmpty()) {
-            throw new UploadFileException("File upload error, file is empty");
-        }
-        String name = file.getOriginalFilename();
-        byte[] bytes = file.getBytes();
-        //create dir
-        String rootPath = System.getProperty("catalina.home");
-        File dir = new File(rootPath + File.separator + "external_uploads");
-        if (!dir.exists()) {
-            if (!dir.mkdir()) {
-                logger.info("directory is not created");
-                throw new UploadFileException("Directory is not created");
-            }
-        }
-        //create file
-        File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
-        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-        stream.write(bytes);
-        stream.close();
-        logger.info("new file saved at: " + serverFile.getAbsolutePath());
-
+        fileService.uploadFile(file);
         return "redirect:/books/shelf";
+    }
+
+    @ResponseBody
+    @PostMapping("/downloadFile")
+    public ResponseEntity<byte[]> downloadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        if (Objects.isNull(file) || file.isEmpty()) {
+            throw new UploadFileException("File download error, file is empty. Choose the correct file, please");
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(file.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                .body(file.getBytes());
     }
 }
---
+++ src/main/java/org/app/repository/ProcessFileService.java
@@ -0,0 +1,40 @@
+package org.app.repository;
+
+import org.apache.log4j.Logger;
+import org.app.exception.UploadFileException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+@Service
+public class ProcessFileService {
+    private final Logger logger = Logger.getLogger(ProcessFileService.class);
+
+    public void uploadFile(MultipartFile file) throws IOException {
+        if (Objects.isNull(file) || file.isEmpty()) {
+            throw new UploadFileException("File upload error, file is empty");
+        }
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+        //create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath + File.separator + "external_uploads");
+        if (!dir.exists()) {
+            if (!dir.mkdir()) {
+                logger.info("directory is not created");
+                throw new UploadFileException("Directory is not created");
+            }
+        }
+        //create file
+        File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+        stream.write(bytes);
+        stream.close();
+        logger.info("new file saved at: " + serverFile.getAbsolutePath());
+    }
+}
