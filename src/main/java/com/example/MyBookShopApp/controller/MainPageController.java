993f633c8c13894931f58c3dbaf8b34af728f005
Shpinkova_LA <liza.shpinkovaa@mail.ru>
2021-09-08 14:27:30 +0300

    added output of all books from db

---
+++ src/main/java/com/example/MyBookShopApp/controller/MainPageController.java
@@ -0,0 +1,26 @@
+package com.example.MyBookShopApp.controller;
+
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("bookData", bookService.getBooksData());
+        return "index";
+    }
+}
