883ab66b75e04454317d7b27f8d2f3f45e38ae92
Shpinkova_LA <liza.shpinkovaa@mail.ru>
2021-09-13 10:53:55 +0300

    refactor sql files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,51 +1,77 @@
-insert into shop.authors(name) values('Drucie Goodbourn'),
-                                      ('Marcia Aggiss'),
-                                      ('Giovanni Bigg'),
-                                      ('Duffie Eynon'),
-                                      ('Anselma McKennan'),
-                                      ('Gert Kira'),
-                                      ('Clerc Djakovic'),
-                                      ('Stacee Elsy'),
-                                      ('Dorotea Bonhill'),
-                                      ('Minni Bunney'),
-                                      ('Berty Gandley'),
-                                      ('Raynard Tesdale'),
-                                      ('Joanna Hutfield'),
-                                      ('Dannie Glidder'),
-                                      ('Timmie Adshede'),
-                                      ('Fernando Neads'),
-                                      ('Teddy Guilliland'),
-                                      ('Paloma Grebert'),
-                                      ('Kristos Usher'),
-                                      ('Darcey Rohmer'),
-                                      ('Cristie Isacsson'),
-                                      ('Lindsay Coutthart'),
-                                      ('Helyn Schneidar'),
-                                      ('Bette-ann Askem'),
-                                      ('Dom Robuchon');
+insert into shop.authors(name)
+values ('Drucie Goodbourn'),
+       ('Marcia Aggiss'),
+       ('Giovanni Bigg'),
+       ('Duffie Eynon'),
+       ('Anselma McKennan'),
+       ('Gert Kira'),
+       ('Clerc Djakovic'),
+       ('Stacee Elsy'),
+       ('Dorotea Bonhill'),
+       ('Minni Bunney'),
+       ('Berty Gandley'),
+       ('Raynard Tesdale'),
+       ('Joanna Hutfield'),
+       ('Dannie Glidder'),
+       ('Timmie Adshede'),
+       ('Fernando Neads'),
+       ('Teddy Guilliland'),
+       ('Paloma Grebert'),
+       ('Kristos Usher'),
+       ('Darcey Rohmer'),
+       ('Cristie Isacsson'),
+       ('Lindsay Coutthart'),
+       ('Helyn Schneidar'),
+       ('Bette-ann Askem'),
+       ('Dom Robuchon');
 
-insert into shop.books (title, price_old, price, author_id) values ('exploit real-time e-commerce', '$203.23', '$43.37', 1);
-insert into shop.books (title, price_old, price, author_id) values ('utilize real-time interfaces', '$260.31', '$35.96', 2);
-insert into shop.books (title, price_old, price, author_id) values ('extend strategic e-services', '$272.00', '$151.04', 3);
-insert into shop.books (title, price_old, price, author_id) values ('iterate integrated solutions', '$271.58', '$141.40', 4);
-insert into shop.books (title, price_old, price, author_id) values ('cultivate magnetic e-commerce', '$278.43', '$54.44', 5);
-insert into shop.books (title, price_old, price, author_id) values ('architect vertical ROI', '$282.54', '$102.61', 6);
-insert into shop.books (title, price_old, price, author_id) values ('disintermediate extensible architectures', '$295.38', '$181.27', 7);
-insert into shop.books (title, price_old, price, author_id) values ('extend leading-edge methodologies', '$283.08', '$97.96', 8);
-insert into shop.books (title, price_old, price, author_id) values ('integrate one-to-one systems', '$258.51', '$52.25', 9);
-insert into shop.books (title, price_old, price, author_id) values ('revolutionize 24/7 infrastructures', '$260.79', '$33.69', 10);
-insert into shop.books (title, price_old, price, author_id) values ('enhance innovative e-services', '$256.54', '$160.98', 11);
-insert into shop.books (title, price_old, price, author_id) values ('maximize 24/365 e-tailers', '$262.07', '$34.06', 12);
-insert into shop.books (title, price_old, price, author_id) values ('scale integrated architectures', '$257.66', '$32.97', 13);
-insert into shop.books (title, price_old, price, author_id) values ('e-enable frictionless web services', '$288.19', '$90.52', 14);
-insert into shop.books (title, price_old, price, author_id) values ('strategize B2B channels', '$232.27', '$44.83', 15);
-insert into shop.books (title, price_old, price, author_id) values ('syndicate compelling deliverables', '$287.45', '$101.89', 16);
-insert into shop.books (title, price_old, price, author_id) values ( 'utilize bleeding-edge users', '$281.37', '$146.21', 17);
-insert into shop.books (title, price_old, price, author_id) values ('cultivate 24/365 networks', '$270.99', '$184.76', 18);
-insert into shop.books (title, price_old, price, author_id) values ('embrace plug-and-play platforms', '$278.36', '$77.71', 19);
-insert into shop.books (title, price_old, price, author_id) values ('target innovative paradigms', '$298.94', '$130.01', 20);
-insert into shop.books (title, price_old, price, author_id) values ('generate open-source e-business', '$276.57', '$113.25', 21);
-insert into shop.books (title, price_old, price, author_id) values ('implement user-centric e-markets', '$254.70', '$115.31', 22);
-insert into shop.books (title, price_old, price, author_id) values ('utilize magnetic supply-chains', '$279.70', '$77.86', 23);
-insert into shop.books (title, price_old, price, author_id) values ('e-enable dot-com supply-chains', '$297.83', '$110.15', 24);
-insert into shop.books (title, price_old, price, author_id) values ('facilitate efficient eyeballs', '$232.30', '$131.48', 25);
+insert into shop.books (title, price_old, price, author_id)
+values ('exploit real-time e-commerce', '$203.23', '$43.37', 1);
+insert into shop.books (title, price_old, price, author_id)
+values ('utilize real-time interfaces', '$260.31', '$35.96', 2);
+insert into shop.books (title, price_old, price, author_id)
+values ('extend strategic e-services', '$272.00', '$151.04', 3);
+insert into shop.books (title, price_old, price, author_id)
+values ('iterate integrated solutions', '$271.58', '$141.40', 4);
+insert into shop.books (title, price_old, price, author_id)
+values ('cultivate magnetic e-commerce', '$278.43', '$54.44', 5);
+insert into shop.books (title, price_old, price, author_id)
+values ('architect vertical ROI', '$282.54', '$102.61', 6);
+insert into shop.books (title, price_old, price, author_id)
+values ('disintermediate extensible architectures', '$295.38', '$181.27', 7);
+insert into shop.books (title, price_old, price, author_id)
+values ('extend leading-edge methodologies', '$283.08', '$97.96', 8);
+insert into shop.books (title, price_old, price, author_id)
+values ('integrate one-to-one systems', '$258.51', '$52.25', 9);
+insert into shop.books (title, price_old, price, author_id)
+values ('revolutionize 24/7 infrastructures', '$260.79', '$33.69', 10);
+insert into shop.books (title, price_old, price, author_id)
+values ('enhance innovative e-services', '$256.54', '$160.98', 11);
+insert into shop.books (title, price_old, price, author_id)
+values ('maximize 24/365 e-tailers', '$262.07', '$34.06', 12);
+insert into shop.books (title, price_old, price, author_id)
+values ('scale integrated architectures', '$257.66', '$32.97', 13);
+insert into shop.books (title, price_old, price, author_id)
+values ('e-enable frictionless web services', '$288.19', '$90.52', 14);
+insert into shop.books (title, price_old, price, author_id)
+values ('strategize B2B channels', '$232.27', '$44.83', 15);
+insert into shop.books (title, price_old, price, author_id)
+values ('syndicate compelling deliverables', '$287.45', '$101.89', 16);
+insert into shop.books (title, price_old, price, author_id)
+values ('utilize bleeding-edge users', '$281.37', '$146.21', 17);
+insert into shop.books (title, price_old, price, author_id)
+values ('cultivate 24/365 networks', '$270.99', '$184.76', 18);
+insert into shop.books (title, price_old, price, author_id)
+values ('embrace plug-and-play platforms', '$278.36', '$77.71', 19);
+insert into shop.books (title, price_old, price, author_id)
+values ('target innovative paradigms', '$298.94', '$130.01', 20);
+insert into shop.books (title, price_old, price, author_id)
+values ('generate open-source e-business', '$276.57', '$113.25', 21);
+insert into shop.books (title, price_old, price, author_id)
+values ('implement user-centric e-markets', '$254.70', '$115.31', 22);
+insert into shop.books (title, price_old, price, author_id)
+values ('utilize magnetic supply-chains', '$279.70', '$77.86', 23);
+insert into shop.books (title, price_old, price, author_id)
+values ('e-enable dot-com supply-chains', '$297.83', '$110.15', 24);
+insert into shop.books (title, price_old, price, author_id)
+values ('facilitate efficient eyeballs', '$232.30', '$131.48', 25);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,16 +2,18 @@ CREATE SCHEMA shop;
 DROP TABLE IF EXISTS shop.authors;
 DROP TABLE IF EXISTS shop.books;
 
-CREATE TABLE  shop.authors(
-author_id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(250) NOT NULL
+CREATE TABLE shop.authors
+(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(250) NOT NULL
 );
 
-CREATE TABLE  shop.books(
-id INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(250) NOT NULL,
-price_old  VARCHAR(250) DEFAULT NULL,
-price VARCHAR(250) DEFAULT NULL,
-author_id INT,
-FOREIGN KEY (author_id) REFERENCES shop.authors(author_id)
+CREATE TABLE shop.books
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    title     VARCHAR(250) NOT NULL,
+    price_old VARCHAR(250) DEFAULT NULL,
+    price     VARCHAR(250) DEFAULT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES shop.authors (author_id)
 );
