6945fc9518e920824baf7400e43dc2fd4e033c1a
Shpinkova_LA <liza.shpinkovaa@mail.ru>
2021-09-20 15:42:51 +0300

    change h2 to postgresql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,20 @@
-CREATE SCHEMA shop;
+CREATE SCHEMA IF NOT EXISTS shop;
 DROP TABLE IF EXISTS shop.authors;
 DROP TABLE IF EXISTS shop.books;
 
 CREATE TABLE shop.authors
 (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id  BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(50),
     last_name  VARCHAR(50)
 );
 
 CREATE TABLE shop.books
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
+    id        BIGSERIAL PRIMARY KEY,
     title     VARCHAR(250) NOT NULL,
     price_old VARCHAR(250) DEFAULT NULL,
     price     VARCHAR(250) DEFAULT NULL,
-    author_id INT,
+    author_id BIGSERIAL,
     FOREIGN KEY (author_id) REFERENCES shop.authors (author_id)
 );
