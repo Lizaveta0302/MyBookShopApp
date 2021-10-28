61c2c5905a53e60c20b70a2dc374ad2fdfb88f05
Shpinkova_LA <liza.shpinkovaa@mail.ru>
2021-10-28 20:25:59 +0300

    removed unused import

--- src/main/java/com/example/MyBookShopApp/controller/BookController.java
+++ src/main/java/com/example/MyBookShopApp/controller/BookController.java
@@ -2,14 +2,11 @@ package com.example.MyBookShopApp.controller;
 
 import com.example.MyBookShopApp.dto.BooksPageDto;
 import com.example.MyBookShopApp.dto.SearchWordDto;
-import com.example.MyBookShopApp.entity.book.Book;
 import com.example.MyBookShopApp.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/books")
