f69e884a0017004b4c868cab12fd6f949f5729a2
Shpinkova_LA <lshpinkova@ibagroup.eu>
2022-05-02 13:02:57 +0300

    Add Integration with payment service (Robokassa)

--- src/main/java/com/example/bookshop_app/controller/CartController.java
+++ src/main/java/com/example/bookshop_app/controller/CartController.java
@@ -3,14 +3,17 @@ package com.example.bookshop_app.controller;
 import com.example.bookshop_app.dto.SearchWordDto;
 import com.example.bookshop_app.entity.book.Book;
 import com.example.bookshop_app.service.BookService;
+import com.example.bookshop_app.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 @Controller
@@ -20,6 +23,8 @@ public class CartController {
 
     @Autowired
     private BookService bookService;
+    @Autowired
+    private PaymentService paymentService;
 
     @ModelAttribute(name = "bookCart")
     public List<Book> bookCart() {
@@ -211,5 +216,15 @@ public class CartController {
             bookService.updateQuantityInBasket(slug, quantityInBasket - 1);
         }
         return "redirect:/cart";
+    }
+
+    @GetMapping("/pay")
+    public RedirectView handlePay(@CookieValue(value = "cartContents", required = false) String cartContents) throws NoSuchAlgorithmException {
+        cartContents = cartContents.startsWith("/") ? cartContents.substring(1) : cartContents;
+        cartContents = cartContents.endsWith("/") ? cartContents.substring(0, cartContents.length() - 1) : cartContents;
+        String[] cookieSlugs = cartContents.split("/");
+        List<Book> booksFromCookieSlugs = bookService.findBooksBySlugIn(cookieSlugs);
+        String paymentUrl = paymentService.getPaymentUrl(booksFromCookieSlugs);
+        return new RedirectView(paymentUrl);
     }
 }
---
+++ src/main/java/com/example/bookshop_app/service/PaymentService.java
@@ -0,0 +1,35 @@
+package com.example.bookshop_app.service;
+
+import com.example.bookshop_app.entity.book.Book;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Value("${robokassa.merchant.login}")
+    private String merchantLogin;
+
+    @Value("${robokassa.pass.first.test}")
+    private String firstTestPass;
+
+    public String getPaymentUrl(List<Book> booksFromCookieSlugs) throws NoSuchAlgorithmException {
+        Double paymentSumTotal = booksFromCookieSlugs.stream().mapToDouble(Book::discountPrice).sum();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        String invId = "5"; //just for testing TODO order indexing later
+        md.update((merchantLogin + ":" + Double.toString(paymentSumTotal) + ":" + invId + ":" + firstTestPass).getBytes());
+        return "https://auth.robokassa.ru/Merchant/Index.aspx" +
+                "?MerchantLogin=" + merchantLogin +
+                "&IndId=" + invId +
+                "&Culture=ru" +
+                "&Encoding=utf-8" +
+                "&OutSum=" + Double.toString(paymentSumTotal) +
+                "&SignatureValue=" + DatatypeConverter.printHexBinary(md.digest()).toUpperCase() +
+                "&IsTest=1";
+    }
+}
