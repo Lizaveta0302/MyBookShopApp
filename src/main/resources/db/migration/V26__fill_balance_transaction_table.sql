0e22b25f82e314a646a4e5a7b6505b12dbbbc213
Shpinkova_LA <lshpinkova@ibagroup.eu>
2022-05-23 15:36:58 +0300

    Add migration scripts for filling data

---
+++ src/main/resources/db/migration/V26__fill_balance_transaction_table.sql
@@ -0,0 +1,17 @@
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Покупка книги Buckskin Frontier', '2022-05-07 21:53:50.710000', 1, 10, false);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (14, 'Покупка книги Hunger', '2022-05-09 21:53:50.710000', 1, 110, false);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Пополнение с помощью WebMoney', '2022-05-15 21:53:50.710000', 1, 230, true);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Покупка книги Buckskin Frontier', '2022-05-07 21:53:50.710000', 1, 30, false);
+
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Покупка книги Buckskin Frontier', '2022-05-07 21:53:50.710000', 2, 90, false);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (14, 'Покупка книги Hunger', '2022-05-09 21:53:50.710000', 2, 170, false);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Пополнение с помощью WebMoney', '2022-05-15 21:53:50.710000', 2, 430, true);
+insert into shop.balance_transaction (book_id, description, "time", user_id, "value", is_income)
+values (13, 'Покупка книги Buckskin Frontier', '2022-05-07 21:53:50.710000', 2, 130, false);
