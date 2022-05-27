9575613272ae048b29b1de79a00bbd637ac81028
Shpinkova_LA <lshpinkova@ibagroup.eu>
2022-05-27 22:57:33 +0300

    Fix db script

--- src/main/resources/db/migration/V23__fill_users_table.sql
+++ src/main/resources/db/migration/V23__fill_users_table.sql
@@ -1,2 +1,2 @@
-insert into shop.users (id, balance, name, email, password, phone) values (1, 1000, 'Admin', 'admin.admin@mail.ru', '$2a$10$ZktQgtLbPq5MCtp8qbjNpOyWBEsyFbjSbI5JOX7caod91QipwQ', '9031232323');
-insert into shop.users (id, balance, name, email, password, phone) values (2, 500, 'Tester', 'test@mail.org', '$2a$10$ZktQgtLbPq5MCtp8qbjNpOyWBEsyFbjSbI5JOX7caod91QipwQ', '9031232323');
+insert into shop.users (balance, name, email, password, phone) values (1000, 'Admin', 'admin.admin@mail.ru', '$2a$10$ZktQgtLbPq5MCtp8qbjNpOyWBEsyFbjSbI5JOX7caod91QipwQ', '9031232323');
+insert into shop.users (balance, name, email, password, phone) values (500, 'Tester', 'test@mail.org', '$2a$10$ZktQgtLbPq5MCtp8qbjNpOyWBEsyFbjSbI5JOX7caod91QipwQ', '9031232323');
